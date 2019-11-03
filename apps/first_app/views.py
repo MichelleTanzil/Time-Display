0889e012464e2a613fb871f476dbe494b6486d56
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-03 14:31:47 -0800

    completed ninja bonus

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from time import gmtime, strftime
+from datetime import datetime
 
 # Create your views here
 
 def index(request):
     context = {
-        "time": strftime("%b %d, %Y \n %H:%M %p", gmtime())
+        "time": strftime("%b %d, %Y \n %H:%M %p", gmtime()),
+        "now": datetime.now()
     }
     return render(request, 'first_app/index.html', context)
